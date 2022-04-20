a070ee6edb74639eced2e392086dab9a4d7f87de
daiyinlong3 <daiyinlong3@jd.com>
2022-04-20 14:43:43 +0800

    测试indent-blankline

--- lua/conf/indent-blankline.lua
+++ lua/conf/indent-blankline.lua
@@ -5,6 +5,7 @@ if not status_ok then
 end
 
 -- disable indent in file type
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 vim.g.indent_blankline_filetype_exclude = {
     "NvimTree",
     "aerial",
@@ -16,10 +17,43 @@ vim.g.indent_blankline_filetype_exclude = {
     "help"
 }
 
+vim.cmd [[highlight IndentBlanklineContextChar guifg=#E06C75 gui=nocombine]]
+
+vim.g.indentLine_enabled = 1
+vim.g.indent_blankline_char = "|"
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_first_indent_level = true
+vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_show_current_context = true
+vim.g.indent_blankline_context_patterns = {
+	"class",
+	"return",
+	"function",
+	"method",
+	"^if",
+	"^while",
+	"jsx_element",
+	"^for",
+	"^object",
+	"^table",
+	"block",
+	"arguments",
+	"if_statement",
+	"else_clause",
+	"jsx_element",
+	"jsx_self_closing_element",
+	"try_statement",
+	"catch_clause",
+	"import_statement",
+	"operation_type",
+}
+
+vim.wo.colorcolumn = "99999"
+
 indent_blankline.setup(
     {
         show_current_context = true,
-        show_current_context_start = true,
-        show_end_of_line = true
+        --show_current_context_start = true,
+        --show_end_of_line = true
     }
 )
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/daiyinlong/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/daiyinlong/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/daiyinlong/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/daiyinlong/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/daiyinlong/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/daiyinlong3/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/daiyinlong3/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/daiyinlong3/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/daiyinlong3/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/daiyinlong3/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -72,12 +72,12 @@ _G.packer_plugins = {
   catppuccin = {
     config = { 'require("conf.catppuccin")' },
     loaded = true,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/start/catppuccin",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/start/catppuccin",
     url = "https://github.com/catppuccin/nvim"
   },
   ["filetype.nvim"] = {
     loaded = true,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/start/filetype.nvim",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/start/filetype.nvim",
     url = "https://github.com/nathom/filetype.nvim"
   },
   ["gitsigns.nvim"] = {
@@ -89,11 +89,11 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/gitsigns.nvim",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["impatient.nvim"] = {
-    after = { "nvim-ts-rainbow", "nvim-hlslens", "nvim-autopairs", "nvim-treesitter", "nvim-lastplace", "indent-blankline.nvim", "plenary.nvim", "nvim-web-devicons" },
+    after = { "plenary.nvim", "nvim-autopairs", "nvim-ts-rainbow", "indent-blankline.nvim", "nvim-treesitter", "nvim-web-devicons", "nvim-lastplace", "nvim-hlslens" },
     loaded = true,
     only_config = true
   },
@@ -102,7 +102,7 @@ _G.packer_plugins = {
     load_after = {},
     loaded = true,
     needs_bufread = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/indent-blankline.nvim",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["lualine.nvim"] = {
@@ -113,7 +113,7 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/lualine.nvim",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["nvim-autopairs"] = {
@@ -122,7 +122,7 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/nvim-autopairs",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-gps"] = {
@@ -132,7 +132,7 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/nvim-gps",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/nvim-gps",
     url = "https://github.com/SmiteshP/nvim-gps"
   },
   ["nvim-hlslens"] = {
@@ -140,7 +140,7 @@ _G.packer_plugins = {
     load_after = {},
     loaded = true,
     needs_bufread = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/nvim-hlslens",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/nvim-hlslens",
     url = "https://github.com/kevinhwang91/nvim-hlslens"
   },
   ["nvim-lastplace"] = {
@@ -149,7 +149,7 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/nvim-lastplace",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/nvim-lastplace",
     url = "https://github.com/ethanholz/nvim-lastplace"
   },
   ["nvim-tree.lua"] = {
@@ -157,7 +157,7 @@ _G.packer_plugins = {
     load_after = {},
     loaded = true,
     needs_bufread = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/nvim-tree.lua",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
@@ -167,7 +167,7 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/nvim-treesitter",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-ts-rainbow"] = {
@@ -175,7 +175,7 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/nvim-ts-rainbow",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/nvim-ts-rainbow",
     url = "https://github.com/p00f/nvim-ts-rainbow"
   },
   ["nvim-web-devicons"] = {
@@ -183,12 +183,12 @@ _G.packer_plugins = {
     load_after = {},
     loaded = true,
     needs_bufread = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
@@ -197,7 +197,7 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/Users/daiyinlong/.local/share/nvim/site/pack/packer/opt/plenary.nvim",
+    path = "/Users/daiyinlong3/.local/share/nvim/site/pack/packer/opt/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   }
 }
@@ -213,6 +213,11 @@ require("conf.catppuccin")
 time([[Config for catppuccin]], false)
 -- Load plugins in order defined by `after`
 time([[Sequenced loading]], true)
+vim.cmd [[ packadd nvim-hlslens ]]
+
+-- Config for: nvim-hlslens
+require("conf.nvim-hlslens")
+
 vim.cmd [[ packadd nvim-web-devicons ]]
 vim.cmd [[ packadd nvim-tree.lua ]]
 
@@ -224,19 +229,14 @@ vim.cmd [[ packadd indent-blankline.nvim ]]
 -- Config for: indent-blankline.nvim
 require("conf.indent-blankline")
 
-vim.cmd [[ packadd nvim-hlslens ]]
-
--- Config for: nvim-hlslens
-require("conf.nvim-hlslens")
-
 time([[Sequenced loading]], false)
 vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Event lazy-loads
 time([[Defining lazy-load event autocommands]], true)
-vim.cmd [[au BufNewFile * ++once lua require("packer.load")({'nvim-ts-rainbow', 'nvim-treesitter', 'nvim-lastplace', 'plenary.nvim'}, { event = "BufNewFile *" }, _G.packer_plugins)]]
+vim.cmd [[au BufNewFile * ++once lua require("packer.load")({'plenary.nvim', 'nvim-ts-rainbow', 'nvim-treesitter', 'nvim-lastplace'}, { event = "BufNewFile *" }, _G.packer_plugins)]]
 vim.cmd [[au InsertEnter * ++once lua require("packer.load")({'nvim-autopairs'}, { event = "InsertEnter *" }, _G.packer_plugins)]]
-vim.cmd [[au BufRead * ++once lua require("packer.load")({'nvim-ts-rainbow', 'nvim-treesitter', 'nvim-lastplace', 'plenary.nvim'}, { event = "BufRead *" }, _G.packer_plugins)]]
+vim.cmd [[au BufRead * ++once lua require("packer.load")({'plenary.nvim', 'nvim-ts-rainbow', 'nvim-treesitter', 'nvim-lastplace'}, { event = "BufRead *" }, _G.packer_plugins)]]
 time([[Defining lazy-load event autocommands]], false)
 vim.cmd("augroup END")
 if should_profile then save_profiles() end
